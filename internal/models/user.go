567008fa9dcd73c70e6a4abdaa88fb066103cce5
Dustin <djourdan555@gmail.com>
2022-07-10 15:06:22 +0700

    add user model methods

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,9 +1,11 @@
 package models
 
 import (
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // User model
@@ -16,4 +18,36 @@ type User struct {
 	Password  string    `json:"password,omitempty" db:"password"`
 	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at,omitempty" db:"updated_at"`
+}
+
+func (u *User) SanitizePassword() {
+	u.Password = ""
+}
+
+func (u *User) HashPassword() error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) ComparePasswords(password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+}
+
+func (u *User) PrepareCreate() error {
+	u.Email = strings.ToLower(strings.TrimSpace(u.Email))
+	u.Password = strings.TrimSpace(u.Password)
+
+	if err := u.HashPassword(); err != nil {
+		return err
+	}
+
+	if u.Role != "" {
+		u.Role = strings.ToLower(strings.TrimSpace(u.Role))
+	}
+
+	return nil
 }
