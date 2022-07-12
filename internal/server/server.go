557989345e1b208359b98cce90940c23306ab741
Dustin Jourdan <djourdan555@gmail.com>
2022-07-12 06:27:25 +0000

    fix query bug and server setup

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,31 +6,20 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
+	"github.com/go-playground/validator"
 	"github.com/go-redis/redis/v8"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
-	"google.golang.org/grpc/reflection"
-
-	grpcrecovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
-	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-
-	"github.com/go-playground/validator"
 
 	"github.com/dinorain/useraja/config"
-	"github.com/dinorain/useraja/internal/interceptors"
 	"github.com/dinorain/useraja/internal/middlewares"
 	sessRepository "github.com/dinorain/useraja/internal/session/repository"
 	sessUseCase "github.com/dinorain/useraja/internal/session/usecase"
-	authServerGRPC "github.com/dinorain/useraja/internal/user/delivery/grpc/service"
 	userDeliveryHTTP "github.com/dinorain/useraja/internal/user/delivery/http/service"
 	userRepository "github.com/dinorain/useraja/internal/user/repository"
 	userUseCase "github.com/dinorain/useraja/internal/user/usecase"
 	"github.com/dinorain/useraja/pkg/logger"
-	userService "github.com/dinorain/useraja/proto"
 )
 
 type Server struct {
@@ -60,7 +49,6 @@ func (s *Server) Run() error {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
 	defer cancel()
 
-	im := interceptors.NewInterceptorManager(s.logger, s.cfg)
 	s.mw = middlewares.NewMiddlewareManager(s.logger, s.cfg)
 	userRepo := userRepository.NewUserPGRepository(s.db)
 	sessRepo := sessRepository.NewSessionRepository(s.redisClient, s.cfg)
@@ -68,7 +56,7 @@ func (s *Server) Run() error {
 	userUC := userUseCase.NewUserUseCase(s.cfg, s.logger, userRepo, userRedisRepo)
 	sessUC := sessUseCase.NewSessionUseCase(sessRepo, s.cfg)
 
-	l, err := net.Listen("tcp", s.cfg.Http.Port)
+	l, err := net.Listen("tcp", s.cfg.Server.Port)
 	if err != nil {
 		return err
 	}
@@ -83,98 +71,11 @@ func (s *Server) Run() error {
 			cancel()
 		}
 	}()
-	s.logger.Infof("API Gateway is listening on PORT: %s", s.cfg.Http.Port)
 
 	<-ctx.Done()
 	if err := s.echo.Server.Shutdown(ctx); err != nil {
 		s.logger.WarnMsg("echo.Server.Shutdown", err)
 	}
-
-	server := grpc.NewServer(grpc.KeepaliveParams(keepalive.ServerParameters{
-		MaxConnectionIdle: s.cfg.Server.MaxConnectionIdle * time.Minute,
-		Timeout:           s.cfg.Server.Timeout * time.Second,
-		MaxConnectionAge:  s.cfg.Server.MaxConnectionAge * time.Minute,
-		Time:              s.cfg.Server.Timeout * time.Minute,
-	}),
-		grpc.UnaryInterceptor(im.Logger),
-		grpc.ChainUnaryInterceptor(
-			grpc_ctxtags.UnaryServerInterceptor(),
-			grpcrecovery.UnaryServerInterceptor(),
-		),
-	)
-
-	if s.cfg.Server.Mode != "Production" {
-		reflection.Register(server)
-	}
-
-	authGRPCServer := authServerGRPC.NewAuthServerGRPC(s.logger, s.cfg, userUC, sessUC)
-	userService.RegisterUserServiceServer(server, authGRPCServer)
-
-	go func() {
-		s.logger.Infof("Server is listening on port: %v", s.cfg.Server.Port)
-		if err := server.Serve(l); err != nil {
-			s.logger.Fatal(err)
-		}
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-
-	<-quit
-	server.GracefulStop()
-	s.logger.Info("Server Exited Properly")
-
-	return nil
-}
-
-// Run GRPC service
-func (s *Server) RunGRPC() error {
-	im := interceptors.NewInterceptorManager(s.logger, s.cfg)
-	userRepo := userRepository.NewUserPGRepository(s.db)
-	sessRepo := sessRepository.NewSessionRepository(s.redisClient, s.cfg)
-	userRedisRepo := userRepository.NewUserRedisRepo(s.redisClient, s.logger)
-	userUC := userUseCase.NewUserUseCase(s.cfg, s.logger, userRepo, userRedisRepo)
-	sessUC := sessUseCase.NewSessionUseCase(sessRepo, s.cfg)
-
-	l, err := net.Listen("tcp", s.cfg.Server.Port)
-	if err != nil {
-		return err
-	}
-	defer l.Close()
-
-	server := grpc.NewServer(grpc.KeepaliveParams(keepalive.ServerParameters{
-		MaxConnectionIdle: s.cfg.Server.MaxConnectionIdle * time.Minute,
-		Timeout:           s.cfg.Server.Timeout * time.Second,
-		MaxConnectionAge:  s.cfg.Server.MaxConnectionAge * time.Minute,
-		Time:              s.cfg.Server.Timeout * time.Minute,
-	}),
-		grpc.UnaryInterceptor(im.Logger),
-		grpc.ChainUnaryInterceptor(
-			grpc_ctxtags.UnaryServerInterceptor(),
-			grpcrecovery.UnaryServerInterceptor(),
-		),
-	)
-
-	if s.cfg.Server.Mode != "Production" {
-		reflection.Register(server)
-	}
-
-	authGRPCServer := authServerGRPC.NewAuthServerGRPC(s.logger, s.cfg, userUC, sessUC)
-	userService.RegisterUserServiceServer(server, authGRPCServer)
-
-	go func() {
-		s.logger.Infof("Server is listening on port: %v", s.cfg.Server.Port)
-		if err := server.Serve(l); err != nil {
-			s.logger.Fatal(err)
-		}
-	}()
-
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
-
-	<-quit
-	server.GracefulStop()
-	s.logger.Info("Server Exited Properly")
 
 	return nil
 }
--- internal/user/repository/sql_queries.go
+++ internal/user/repository/sql_queries.go
@@ -1,18 +1,18 @@
 package repository
 
 const (
-	createUserQuery = `INSERT INTO users (first_name, last_name, email, password, role, avatar, delivery_address) 
+	createUserQuery = `INSERT INTO users (first_name, last_name, email, password, role, avatar) 
 		VALUES ($1, $2, $3, $4, $5, COALESCE(NULLIF($6, ''), null, $7)) 
-		RETURNING user_id, first_name, last_name, email, password, avatar, created_at, updated_at, role, delivery_address`
+		RETURNING user_id, first_name, last_name, email, password, avatar, created_at, updated_at, role`
 
-	findByEmailQuery = `SELECT user_id, email, first_name, last_name, role, avatar, password, delivery_address, created_at, updated_at FROM users WHERE email = $1`
+	findByEmailQuery = `SELECT user_id, email, first_name, last_name, role, avatar, password, created_at, updated_at FROM users WHERE email = $1`
 
-	findByIDQuery = `SELECT user_id, email, first_name, last_name, role, avatar, password, delivery_address, created_at, updated_at FROM users WHERE user_id = $1`
+	findByIDQuery = `SELECT user_id, email, first_name, last_name, role, avatar, password, created_at, updated_at FROM users WHERE user_id = $1`
 
-	findAllQuery = `SELECT user_id, email, first_name, last_name, role, avatar, password, delivery_address, created_at, updated_at FROM users LIMIT $1 OFFSET $2`
+	findAllQuery = `SELECT user_id, email, first_name, last_name, role, avatar, password, created_at, updated_at FROM users LIMIT $1 OFFSET $2`
 
-	updateByIDQuery = `UPDATE users SET first_name = $2, last_name = $3, email = $4, password = $5, role = $6, avatar = $7, delivery_address = $8 WHERE user_id = $1
-		RETURNING user_id, first_name, last_name, email, password, avatar, delivery_address, created_at, updated_at, role`
+	updateByIDQuery = `UPDATE users SET first_name = $2, last_name = $3, email = $4, password = $5, role = $6, avatar = $7 = $8 WHERE user_id = $1
+		RETURNING user_id, first_name, last_name, email, password, avatar, created_at, updated_at, role`
 
 	deleteByIDQuery = `DELETE FROM users WHERE user_id = $1`
 )
